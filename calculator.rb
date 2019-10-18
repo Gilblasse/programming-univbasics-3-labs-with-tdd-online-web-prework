80578dc58daad84fd6f77cef493a983a082aa217
Nethelbert <gilblasse@gmail.com>
2019-10-18 13:44:32 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,10 @@
+first_number = 1
+second_number = 1
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
+
 def calculator (num1,num2,operator)
   first_number = num1
   second_number = num2
@@ -20,5 +27,3 @@ def calculator (num1,num2,operator)
   end
   
 end
-
-calculator(1,2,'/')
