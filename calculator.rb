8425c02e32e6836077f7d6d1351974c55f8797bb
Nethelbert <gilblasse@gmail.com>
2019-10-18 13:43:44 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,10 +1,3 @@
-first_number = 1
-second_number = 1
-sum = first_number + second_number
-difference = first_number - second_number
-product = first_number * second_number
-quotient = first_number / second_number
-
 def calculator (num1,num2,operator)
   first_number = num1
   second_number = num2
@@ -16,7 +9,14 @@ def calculator (num1,num2,operator)
   case operator
     when '+'
       return sum
-    when 
+    when '-'
+      return difference
+    when '*'
+      return product
+    when '/'
+      return quotient
+    else
+      return "invalid operator"
   end
   
 end
