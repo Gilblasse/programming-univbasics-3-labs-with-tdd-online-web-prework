9da2dd297e0e2293122782a4e73d1fb5ca89477e
Nethelbert <gilblasse@gmail.com>
2019-10-18 13:41:32 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -8,7 +8,15 @@ quotient = first_number / second_number
 def calculator (num1,num2,operator)
   first_number = num1
   second_number = num2
+  sum = first_number + second_number
+  difference = first_number - second_number
+  product = first_number * second_number
+  quotient = first_number / second_number
   
-  if operator == 'x' or 
+  case operator
+    when '+'
+      return sum
+    when 
+  end
   
 end
