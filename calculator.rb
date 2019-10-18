9379e6cbbb715ca3629c041fa6fe4c90087305c2
Nethelbert <gilblasse@gmail.com>
2019-10-18 13:46:52 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -5,28 +5,4 @@ sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
 quotient = first_number / second_number
-=end
 
-def calculator (num1,num2,operator)
-  first_number = num1
-  second_number = num2
-  sum = first_number + second_number
-  difference = first_number - second_number
-  product = first_number * second_number
-  quotient = first_number / second_number
-  
-  case operator
-    when '+'
-      return sum
-    when '-'
-      return difference
-    when '*'
-      return product
-    when '/'
-      return quotient
-    else
-      return "invalid operator"
-  end
-end
-
-calculator(4,2,'/')
