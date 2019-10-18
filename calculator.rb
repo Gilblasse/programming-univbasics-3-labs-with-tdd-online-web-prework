7c00b59fd04c7c7b72a14d7b4d2f947aa8a1b205
Nethelbert <gilblasse@gmail.com>
2019-10-18 13:36:08 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,3 +4,9 @@ sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
 quotient = first_number / second_number
+
+def calculator (num1,num2,operator)
+  first_number = num1
+  second_number = num2
+  operator
+end
